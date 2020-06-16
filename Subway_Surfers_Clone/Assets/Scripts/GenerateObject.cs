a0015a43501ef97d079374eea14660fa15ab42ec
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2020-06-16 13:28:03 +0100

    Lamp posts/Lighting/Scripting!
    1)Added lamp posts to game to enrich visuals of the game and to use them later, when I bring day night cycle.
    
    2) Added lighting to car and lamp posts.
    
    3)Changed generateObjects script. I moved object list and object chances to spawn to separate script and attached it to SpawnPoints parent gameObject. That way its much easier to change values in inspector, as previously it required to handpick all points manually to change their values.

--- Subway_Surfers_Clone/Assets/Scripts/GenerateObject.cs
+++ Subway_Surfers_Clone/Assets/Scripts/GenerateObject.cs
@@ -5,35 +5,59 @@ using UnityEngine.SocialPlatforms;
 public class GenerateObject : MonoBehaviour
 {
     // Variables
-    [SerializeField] private List<GameObject> objectList; // List that stores objects, e.g., coins, powerups.
-    [SerializeField] private int objectIndex; // Used to pick up object from list.
-    // Object chances to spawn
-    [Header("Chances to spawn.")]
-    [SerializeField] private int Coin = 0;
-    [SerializeField] private int Wall = 0;
-    private int previousRangeMax; // Used to store max value of previous range check
+    private int objectIndex;  // Used to pick up object from list.
+    private enum ObtectType
+    {
+        Coin = 1,
+        Wall = 2,
+        LampPost = 3
+    }
+    private SpawnChances _object;
 
     // Start is called before the first frame update
     void Start()
     {
+        _object = FindObjectOfType<SpawnChances>();
         // Object Generation routine 
-        int rand = Random.Range(0, 100); // Generate random number to decide, which object to spawn.
+        // Generate random number to decide, which object to spawn.
+        int rand = Random.Range(0, 100); 
         // Define chances of objects to be spawned 
         // Coin
-        if (rand >= 0 && rand <= Coin)
+        if (rand > 0 && rand <= _object.getChanceValue((int)ObtectType.Coin))
         {
             objectIndex = 1;
         }
         // Wall
         if (gameObject.transform.name == "WallPoint")
         {
-            if (rand >= 0 && rand <= Wall)
+            if (rand > 0 && rand <= _object.getChanceValue((int)ObtectType.Wall))
             {
                 objectIndex = 2;
             }
         }
-        GameObject instance = Instantiate(objectList[objectIndex], transform.position, Quaternion.identity); // Spawn object at position of game object, which has this script attached to.
-        instance.transform.position = new Vector3(transform.position.x, objectList[objectIndex].transform.position.y, transform.position.z); // Change y coordinate of spawned object
-        instance.transform.parent = transform; // Make spawned object a child of spawn point. That way spawned objects will be destroyed along with spawn point, when destroying a chunk.
+        //Lamp post left side
+        if (gameObject.transform.name == "SidePointL")
+        {
+            if (rand > 0 && rand <= _object.getChanceValue((int)ObtectType.LampPost))
+            {
+                objectIndex = 3;
+            }
+        }
+        //Lamp post right side
+        if (gameObject.transform.name == "SidePointR")
+        {
+            if (rand > 0 && rand <= _object.getChanceValue((int)ObtectType.LampPost))
+            {
+                objectIndex = 4;
+            }
+        }
+        // Spawn object at position of game object, which has this script attached to.
+        GameObject instance = 
+            Instantiate(_object.returnList()[objectIndex], transform.position, _object.returnList()[objectIndex].transform.rotation); 
+        // Change y coordinate of spawned object
+        instance.transform.position = 
+            new Vector3(transform.position.x, _object.returnList()[objectIndex].transform.position.y, transform.position.z); 
+        // Make spawned object a child of spawn point. That way spawned objects will be destroyed along with spawn point, when destroying a chunk.
+        instance.transform.parent = transform; 
     }
 }
---
+++ Subway_Surfers_Clone/Assets/Scripts/SpawnChances.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class SpawnChances : MonoBehaviour
+{
+    // Object chances to spawn
+    [Header("Chances to spawn.")]
+    [SerializeField] private int Coin = 0;
+    [SerializeField] private int Wall = 0;
+    [SerializeField] private int LampPost = 0;
+
+    [SerializeField] private List<GameObject> objectList; // List that stores objects, e.g., coins, powerups.
+   
+    //Returns chance value to caller
+    public int getChanceValue(int index)
+    {
+        switch(index)
+        {
+            default:
+                return 0;
+            case 1:
+                return Coin;
+            case 2:
+                return Wall;
+            case 3:
+                return LampPost;
+        }
+    }
+
+    //Returns list to caller
+    public List<GameObject> returnList()
+    {
+        return objectList;
+    }
+}
