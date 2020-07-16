37f5395c00a63b12b7155f4e791a025ec6f662a0
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2020-07-16 19:47:18 +0100

    Introduced Day/Night cycle!
    Now there is working daynight cycle.
    Also fixed coin animation bug.

--- Subway_Surfers_Clone/Assets/Scripts/DayNightCycle.cs
+++ Subway_Surfers_Clone/Assets/Scripts/DayNightCycle.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    [SerializeField] private float DayTime;
+    
+    [Range(0, 1)][SerializeField] private float CurrentTime;
     [SerializeField] private float DayDuration;
+    [SerializeField] private AnimationCurve SunCurve;
+    [SerializeField] private float SunIntensity;
+    [Header("Light Source")]
+    [SerializeField] private Light Sun;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SunIntensity = Sun.intensity;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CurrentTime += Time.deltaTime / DayDuration;
+        if (CurrentTime >= 1)
+            CurrentTime -= 1;
+        Sun.transform.localRotation = Quaternion.Euler(CurrentTime * 360f, 180, 0);
+        Sun.intensity = SunIntensity * SunCurve.Evaluate(CurrentTime);
     }
 }
